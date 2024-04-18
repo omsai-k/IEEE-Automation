a5d4057b8821a505776a0f6621f8df77311e56d6
Omsai K <omsaikongara7@gmail.com>
2024-04-18 15:43:50 +0530

    updated the XPATHs with IDs and extended the automation

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+
+timeout = 15
 
 membership_data = "Membership.xlsx"
 df = pd.read_excel(membership_data)
@@ -35,101 +39,119 @@ join_button.click()
 join_as_student = driver.find_element(By.XPATH,"/html/body/div[4]/div/div/div[4]/div/div[3]/div[1]/div[1]/div/div[3]/div[1]/div[2]/div/div[1]/a")
 join_as_student.click()
 
-i = 0
 
-while(True):
-    input("Press ENTER/RETURN after creating an account or logging in to an existing account")
-
-    dept_name = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[1]/div/div[4]/div/input")
+for i in range(len(branches)):
+    # input("Press ENTER/RETURN after creating an account or logging in to an existing account")
+    input()
+    
+    WebDriverWait(driver,timeout).until(EC.presence_of_element_located((By.ID,"organization-name")))
+    sleep(3)
+    dept_name = driver.find_element(By.ID,"organization-name")
     dept_name.send_keys("BMSIT")
-
-    checkbox_1 = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[1]/div/div[5]/div/div/div[2]/div[3]/input")
+    
+    checkbox_1 = driver.find_element(By.ID,"contact-info_customer_addresses_0__addressType_code-3")
     checkbox_1.click()
 
-    address_1 = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[1]/div/div[6]/div[1]/input")
+    address_1 = driver.find_element(By.ID,"address-line1")
     address_1.send_keys("Doddaballapur Main Road,Avalahalli")
 
-    address_2 = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[1]/div/div[7]/div/input")
+    address_2 = driver.find_element(By.ID,"address-line2")
     address_2.send_keys("Yelahanka, Bengaluru, Karnataka 560064")
 
-    city = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[1]/div/div[8]/div[2]/div/input")
+    city = driver.find_element(By.ID,"city")
     city.send_keys("Bengaluru")
 
-    dropdown = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[1]/div/div[8]/div[3]/div/select")
+    dropdown = driver.find_element(By.ID,"province")
     select_state = Select(dropdown)
     select_state.select_by_visible_text("Karnataka")
 
-    pin_code = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[1]/div/div[8]/div[4]/div/input")
+    pin_code = driver.find_element(By.ID,"postal-code")
     pin_code.send_keys("560064")
 
-    telephone = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div/div/div[3]/div[2]")
+    telephone = driver.find_element(By.ID,"mobile-number")
     telephone.send_keys("+917760735073")
 
-    continue_button = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[2]/div[3]/div/div[1]/div/div/div/div/div[6]/input")
+    # continue_button = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[2]/div[3]/div/div[1]/div/div/div/div/div[6]/input")
+
+    WebDriverWait(driver,timeout).until(EC.presence_of_element_located((By.ID,"mc-pr-checkout-sec")))
+    
+    continue_button = driver.find_element(By.ID,"mc-pr-checkout-sec")
     continue_button.click()
 
-    search_college = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[2]/div[3]/input")
+    sleep(5)
+
+    search_college = driver.find_element(By.ID,"searchUniversity")
     search_college.click()
 
-    search_box = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[3]/div/div/div/div[1]/div[2]/div[2]/div/input")
-    search_box.send_keys("BMS")
-    sleep(2)
+    WebDriverWait(driver,timeout).until(EC.presence_of_element_located((By.ID,"universityFilter")))
 
-    bmsit = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[3]/div/div/div/div[1]/div[5]/div[1]/a")
+    search_box = driver.find_element(By.ID,"universityFilter")
+    search_box.send_keys("BMS")
+    
+    WebDriverWait(driver,timeout).until(EC.presence_of_element_located((By.ID,"schoolCol1")))
+
+    bmsit = driver.find_element(By.ID,"schoolCol1")
     bmsit.click()
 
-    undergrad = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[5]/div[2]/div[1]/input")
+    undergrad = driver.find_element(By.ID,"studentStatusUndergraduate Student")
     undergrad.click()
 
-    degree = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[7]/div/select[3]")
+    degree = driver.find_element(By.ID,"stud-degree-pursued")
     select_degree = Select(degree)
     select_degree.select_by_visible_text("Bachelor of Engineering")
 
-    program = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[10]/div/select")
+    program = driver.find_element(By.ID,"stud-academic-program")
     select_program  = Select(program)
     select_program.select_by_visible_text(full_forms[branches[i]])
 
-    grad_month = driver.find_element("/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[13]/div[1]/select")
+    grad_month = driver.find_element(By.ID,"estimated-grad-month")
     select_month = Select(grad_month)
     select_month.select_by_visible_text("August")
 
-    grad_year = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[13]/div[2]/select")
+    grad_year = driver.find_element(By.ID,"estimated-grad-year")
     select_year = Select(grad_year)
+    print(grad_years[i])
     select_year.select_by_visible_text(grad_years[i])
 
-    field_of_study = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[14]/div/select")
+    field_of_study = driver.find_element(By.ID,"stud-current-study")
     select_field_of_study = Select(field_of_study)
     select_field_of_study.select_by_visible_text("Computer Sciences and Information Technologies")
     
-    tech_focus = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[3]/div[2]/div/div/div[4]/div/div[6]/div/div/div/div[1]/div/form/fieldset/div/div/div[17]/div/select")
+    accreditation = driver.find_element(By.ID,"stud-prog-accredited")
+    select_accreditation = Select(accreditation)
+    select_accreditation.select_by_visible_text("Yes")
+    
+    tech_focus = driver.find_element(By.ID,"stud-technical-focus")
     select_tech_focus = Select(tech_focus)
     select_tech_focus.select_by_visible_text("Computing and Processing (Hardware/Software)")
 
-    mem_dir = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[4]/div[2]/div/div/div[4]/div/div[6]/div/div/form/div[1]/div/div/div/div[1]/div[1]/input")
+    mem_dir = driver.find_element(By.ID,"memberdir-options1")
     mem_dir.click()
 
-    join_puspose = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[4]/div[2]/div/div/div[4]/div/div[6]/div/div/form/div[4]/div")
-    check_boxes = join_puspose.find_element(By.XPATH,".//input[@type='checkbox']")
+    join_puspose = driver.find_element(By.ID,"additional-info")
+    check_boxes = join_puspose.find_element(By.ID,".//input[@type='checkbox']")
     
     for check_box in check_boxes[:-1]:
         check_box.click()
 
-    referral = driver.find_element(By.XPATH,"/html/body/div[6]/div[4]/div[1]/div[1]/div[2]/div[4]/div[2]/div/div/div[4]/div/div[6]/div/div/form/fieldset/div[1]/div/select")
+    referral = driver.find_element(By.ID,"member-referral")
     select_referral = Select(referral)
     select_referral.select_by_visible_text("Member referral")
 
-    continue_button.click()
+    # continue_button.click()
 
-    society_memberships = driver.find_element(By.XPATH,"/html/body/div[4]/div[4]/div[1]/div[1]/div[5]/div[2]/div/div/div[2]/div/div")
+    input()
+
+    society_memberships = driver.find_element(By.LINK_TEXT,"Society Memberships")
     society_memberships.click()
 
-    comp_society = driver.find_element(By.XPATH,"/html/body/div[4]/div[4]/div[1]/div[1]/div[3]/div[4]/div/div[14]/div[2]/p[1]/a")
+    comp_society = driver.find_element(By.LINK_TEXT,"IEEE Computer Society Membership")
     comp_society.click()
 
-    add_items = driver.find_element(By.XPATH,"/html/body/div[4]/div[4]/div[1]/div[1]/div[6]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div[6]/div/div/div[1]/input")
+    add_items = driver.find_element(By.ID,"add-product-button-id")
     add_items.click()
 
-    catalog = driver.find_element("/html/body/div[4]/div[4]/div[1]/div[1]/div[1]/div[1]/h1/a")
+    catalog = driver.find_element(By.LINK_TEXT,"Catalog")
     catalog.click()
 
     society_memberships.click()
